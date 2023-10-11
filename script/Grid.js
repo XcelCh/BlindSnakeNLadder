6d93e7b72e3575b118d1fb07e20e262d3157d43b
Excel Chendrawan <chendraw002@mymail.sim.edu.sg>
2023-10-12 00:12:31 +0700

    Add setting.html, index.html, game.html

---
+++ script/Grid.js
@@ -0,0 +1,8 @@
+class Grid{
+    constructor(players, snake, eagle, ladder){
+        this.players = players;
+        this.snake = snake;
+        this.eagle = eagle;
+        this.ladder = ladder;
+    }
+}
